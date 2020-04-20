9f6f9042e606152b83b84180bccdbbb15fe66bcd
Marko Kocic <marko@euptera.com>
2020-04-20 16:16:04 +0200

    Bump version

--- project.clj
+++ project.clj
@@ -1,6 +1,7 @@
-(defproject rn-native-base "0.1.1"
+(defproject rn-native-base "0.1.2-SNAPSHOT"
   :description "native-base wrapper for Clojurescript"
   :url "http://github.com/markokocic/rn-native-base"
   :license {:name "MIT"}
+  :deploy-repositories {"releases" {:url "https://repo.clojars.org" :sign-releases false}}
   :dependencies [[org.clojure/clojure "1.10.0"]
                  [org.clojure/clojurescript "1.10.520"]])
