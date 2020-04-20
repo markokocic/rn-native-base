112fbf3c52c475ebbc7b8639524d43e01c2856ff
Marko Kocic <marko@euptera.com>
2020-04-20 13:26:26 +0200

    Initial code

---
+++ project.clj
@@ -0,0 +1,6 @@
+(defproject rn-native-base "0.1.0-SNAPSHOT"
+  :description "native-base wrapper for Clojurescript"
+  :url "http://github.com/markokocic/rn-native-base"
+  :license {:name "MIT"}
+  :dependencies [[org.clojure/clojure "1.10.0"]
+                 [org.clojure/clojurescript "1.10.520"]])
